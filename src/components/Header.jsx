c954f782ce321f39372ec1dcdb4e5414740712b5
TanveenAmbrose <racoctanveen15@gmail.com>
2025-06-21 17:16:56 +0600

    maybe i will do it later

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
       <div className='container text-center mx-auto py-4 px-6 md:px-20 lg:px-32 text-white'>
         <h2 className='text-5xl sm:text-6xl md:text-[82px] inline-block max-w-3xl font-semibold pt-20'>Explore homes that fit your dreams</h2>
         <div className='space-x-6 mt-16'>
-            <a href="#Projects" className='border border-white px-8 py-3 hover:bg-gray-400 hover:text-black  '>Projects</a>
+            <a href="#Projects" className='border border-white px-8 py-3 hover:bg-gray-400 hover:text-black rounded-2xl '>Projects</a>
             <a href="#Contact" className='bg-blue-500 border border-black px-8 py-3 rounded-3xl'>Contact Us</a>
         </div>
       </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,6 +13,7 @@ const Navbar = () => {
             <a href="#Header" className='cursor-pointer hover:text-gray-400'>Testimonials</a>
         </ul>
         <button className='hidden md:block bg-white px-8 py-2 rounded-full'>Sign Up</button>
+        <img src={assets.menu_icon} className='md:hidden w-7' alt="" />
      </div>
     </div>
   )
