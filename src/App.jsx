b9bbfcd5be721089a50ffb7e238d7ffc9ef1cf3e
TanveenAmbrose <racoctanveen15@gmail.com>
2025-06-22 14:54:21 +0600

    bal

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import "./index.css";
 import Header from './components/Header';
 import About from './components/About';
+import Projects from './components/Projects';
 
 const App = () => {
   return (
     <div className='w-full overflow-hidden'>
       <Header/>
       <About/>
+      <Projects/>
     </div>
   )
 }
